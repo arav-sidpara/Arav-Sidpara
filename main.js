823abb6535e1bc50dc7200d97da8394d9370dce0
arav-sidpara <sidparaarav83@gmail.com>
2025-12-18 20:48:26 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 function toggleMode(){
-  document.body.classList.toggle("light");
+    if(document.body.style.background=="#111"){
+        document.body.style.background="#fff";
+        document.body.style.color="#000";
+    }else{
+        document.body.style.background="#111";
+        document.body.style.color="#fff";
+    }
 }
